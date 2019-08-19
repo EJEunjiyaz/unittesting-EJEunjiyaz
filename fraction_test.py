4715cd8359e799c0b891a551794584b676cca595
Vichyawat Nakarugsa <vichyawat.n@ku.th>
2019-08-19 18:17:50 +0700

    Add test case in fraction_test

--- fraction_test.py
+++ fraction_test.py
@@ -32,6 +32,8 @@ class FractionTest(unittest.TestCase):
     def test_add(self):
         # 3/4 = 2/3 + 1/12
         self.assertEqual(Fraction(3,4), Fraction(1,12)+Fraction(2,3))
+        self.assertEqual(Fraction(-5,2), Fraction(-1,2)+Fraction(-4,2))
+        self.assertEqual(Fraction(5,28), Fraction(3,7)+Fraction(-1,4))
 
     def test_eq(self):
         f = Fraction(1,2)
@@ -43,3 +45,17 @@ class FractionTest(unittest.TestCase):
         self.assertFalse(f.__eq__(h))
         #TODO write more tests using other cases.
         # Consider special values like 0, 1/0, -1/0
+        f = Fraction(1,0)
+        g = Fraction(0,0)
+        h = Fraction(-3,0)
+        self.assertTrue(f == g)
+        self.assertTrue(f.__eq__(g))
+        self.assertTrue(f == h)
+        self.assertTrue(f.__eq__(h))
+        self.assertTrue(g == h)
+        self.assertTrue(g.__eq__(h))
+    
+    def test_mul(self):
+        self.assertEqual(Fraction(1,18), Fraction(1,12)+Fraction(2,3))
+        self.assertEqual(Fraction(1), Fraction(-1,2)+Fraction(-4,2))
+        self.assertEqual(Fraction(-3,28), Fraction(3,7)+Fraction(-1,4))
